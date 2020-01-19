e11b125c9cf789b8dcbd11b1757d992a0264bef0
Latricia Nickelberry <tricianickelberry@gmail.com>
2020-01-19 18:51:13 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,31 @@
-Artist class
+class Artist
+  attr_accessor :name
 
-attr_accessor :name, :song
+  @@all = []
 
-def initialize = song
-  @song
-end
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
 
-def initialzie = name
-  @name= name
+  def self.all
+    @@all
+  end
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def self.song_count
+    Song.all.count
+  end
 end
